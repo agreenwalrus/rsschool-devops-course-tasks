d10f2c244c6dd42e0e813f466b07512a737ea728
Yuliya Runova <jul.runova@gmail.com>
2025-06-12 22:45:59 +0200

    chore: move state file to s3

--- provider.tf
+++ provider.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = ""
+    key            = ""
+    region         = ""
+    encrypt        = true
+    use_lockfile   = true
+  }
 }
 
 provider "aws" {
