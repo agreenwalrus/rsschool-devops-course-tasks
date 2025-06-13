fd8952ef5a904f882e4cc63c7a170f7183d599bd
Yuliya Runova <jul.runova@gmail.com>
2025-06-14 00:16:10 +0200

    fix: condition must be like

--- iam_github_actions.tf
+++ iam_github_actions.tf
@@ -25,6 +25,8 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+          },
+          StringLike = {
             "token.actions.githubusercontent.com:sub" : "repo:agreenwalrus/rsschool-devops-course-tasks:*"
           }
         }
